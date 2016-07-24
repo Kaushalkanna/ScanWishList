4a0131b116cccc85af562c4e9994f4e8dc098223
kdontula <kdontula@hawk.iit.edu>
2016-07-24 14:45:57 -0500

    moved click listeners to activity from custom adapter

--- barcode scanner/src/main/java/com/gnirt69/barcodescanner/CustomAdapter.java
+++ barcode scanner/src/main/java/com/gnirt69/barcodescanner/CustomAdapter.java
@@ -5,18 +5,12 @@ package com.gnirt69.barcodescanner;
  */
 
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.app.AlertDialog;
 
 import java.util.List;
 
@@ -58,42 +52,6 @@ public class CustomAdapter extends BaseAdapter {
         ImageView listImage = (ImageView) rowView.findViewById(R.id.listImage);
         listName.setText(result.get(position).getName());
         listPrice.setText(result.get(position).getPrice());
-//        listImage.setImageResource(result.get(position).getPrice());
-        rowView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = result.get(position).getProductUrl();
-                String toastText;
-                if (url!= null &&!url.equals("") && !url.equals("N/A")) {
-                    toastText = "opening "+url+" in browser";
-                    if (!url.startsWith("http://") && !url.startsWith("https://"))
-                        url = "http://" + url;
-                }else{
-                    toastText = "URL not found. Searching for product in google";
-                    url = "https://www.google.com/#tbm=shop&q=" + result.get(position).getUPC();
-                }
-                Toast.makeText(context,toastText,Toast.LENGTH_SHORT).show();
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                context.startActivity(browserIntent);
-            }
-        });
-        rowView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                AlertDialog.Builder adb=new AlertDialog.Builder(context);
-                adb.setTitle("Delete?");
-                adb.setMessage("Are you sure you want to delete " + result.get(position).getName());
-                adb.setNegativeButton("Cancel", null);
-                adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        datasource.deleteItem(result.get(position));
-//                        context.notifyDataSetChanged();
-                        Toast.makeText(context,"I am the one",Toast.LENGTH_SHORT).show();
-                    }});
-                adb.show();
-                return true;
-            }
-        });
         return rowView;
     }
 
--- barcode scanner/src/main/java/com/gnirt69/barcodescanner/ListViewAndroid.java
+++ barcode scanner/src/main/java/com/gnirt69/barcodescanner/ListViewAndroid.java
@@ -1,24 +1,70 @@
 package com.gnirt69.barcodescanner;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.app.AlertDialog;
 
 import java.util.List;
 
 public class ListViewAndroid extends Activity {
     ListView listView;
+    CustomAdapter adapter;
+    ItemsDataSource datasource;
+    List<Item> values;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
         listView = (ListView) findViewById(R.id.list);
-        ItemsDataSource datasource = new ItemsDataSource(this);
+        datasource = new ItemsDataSource(this);
         datasource.open();
-        List<Item> values = datasource.getAllItems();
-        datasource.close();
-        listView.setAdapter(new CustomAdapter(this, values));
+        values = datasource.getAllItems();
+        context = this;
+        adapter = new CustomAdapter(this, values);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, final int position, long id) {
+                String url = values.get(position).getProductUrl();
+                String toastText;
+                if (url!= null &&!url.equals("") && !url.equals("N/A")) {
+                    toastText = "opening "+url+" in browser";
+                    if (!url.startsWith("http://") && !url.startsWith("https://"))
+                        url = "http://" + url;
+                }else{
+                    toastText = "URL not found. Searching for product in google";
+                    url = "https://www.google.com/#tbm=shop&q=" + values.get(position).getUPC();
+                }
+                Toast.makeText(context,toastText,Toast.LENGTH_SHORT).show();
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                context.startActivity(browserIntent);
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            public boolean onItemLongClick(AdapterView<?> a, View v, final int position, long id) {
+                AlertDialog.Builder adb=new AlertDialog.Builder(context);
+                adb.setTitle("Delete?");
+                adb.setMessage("Are you sure you want to delete " + values.get(position).getName());
+                adb.setNegativeButton("Cancel", null);
+                adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        datasource.deleteItem(values.get(position));
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(context,"Deleted " + values.get(position).getName(),Toast.LENGTH_SHORT).show();
+                    }});
+                adb.show();
+                return true;
+            }
+        });
     }
 
 }
